78d7c49a05481328b34990a933e15df33d0375fa
Isaias Israel Zeferino <izeferin@ucsd.edu>
2016-12-14 18:06:04 -0800

    post values set

--- tip_calculator.php
+++ tip_calculator.php
@@ -2,15 +2,32 @@
 <head></head>
 <body>
    <h1 align=center>Tip Calculator</h1>
-   <form align=center action="calculator.php" method="post">
-   Bill Subtotal: $<input type="text" name="subtotal"><br>
+   <form align=center action="tip_calculator.php" method="post">
+   Bill Subtotal: $<input type="text" name="subtotal" value="<?php 
+      if(isset($_POST["subtotal"])) {
+		  echo $_POST["subtotal"];
+	  }
+	  else
+	  {
+		  echo "";
+	  }
+   ?>"><br><br> 
+   Tip Percentage:
    <?php
       for($i=0; $i < 3; $i++) {
+		  $x = 10 + $i*5;
    ?>   
-      <input type="radio" name="percent"> <?php echo 10 + $i*5, "%"; ?>
-
+      <input type="radio" name="percent" value="<?php echo $x; ?>"
+         <?php 
+	        if(isset($_POST["percent"]) && $_POST["percent"] == $x) {
+			   echo ' checked="checked"';
+		    }
+         ?>>
+	  <?php echo $x, "%"; ?>
    <?php   
       }
    ?>
+   <br><br>
+   <input type="submit">
 </body>
 </html>
