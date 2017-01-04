c14507776097a61dca12326ddc01fa7557bac9bc
Isaias Israel Zeferino <izeferin@ucsd.edu>
2017-01-03 20:51:15 -0800

    total calculation working

--- tip_calculator.php
+++ tip_calculator.php
@@ -1,5 +1,15 @@
 <html>
-<head></head>
+<head>
+   <style>
+   div {
+	   width: 200px;
+	   border: 1px solid black;
+	   padding: 20px;
+	   text-align: left;
+	   margin: 0 auto;
+   }
+   </style>
+</head>
 <body>
    <h1 align=center>Tip Calculator</h1>
    <form align=center action="tip_calculator.php" method="post">
@@ -29,5 +39,28 @@
    ?>
    <br><br>
    <input type="submit">
+   <br><br>
+   
+   <?php
+      if(isset($_POST["percent"]) && isset($_POST["subtotal"]) && !empty($_POST["subtotal"])) {
+		  $subtotal = $_POST["subtotal"];
+		  if(is_numeric($subtotal) && $subtotal > 0) {
+   ?>
+            <div>
+	        Tip: <?php
+			         $percent = $_POST["percent"];
+					 $tip = ($percent / 100) * $subtotal;
+					 echo '$', number_format((float)$tip, 2, '.', '');
+			     ?>
+			<br><br>
+	        Total: <?php
+			          $total = $tip + $subtotal;
+					  echo '$', number_format((float)$total, 2, '.', '');
+			       ?>
+	        </div>
+   <?php
+		  }   
+	  }
+   ?>
 </body>
 </html>
